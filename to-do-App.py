eb2e67b7cc27500774f4ccca280bb6895ce424bf
Soham Das <majumder70.ranu@gmail.com>
2020-10-10 19:10:29 +0530

    Added buttons

--- to-do-App.py
+++ to-do-App.py
@@ -21,4 +21,20 @@ list_add_button = tkinter.Button(input_frame, text='Add', bg="#DCCDE8", borderwi
 list_entry.grid(row=0, column=0)
 list_add_button.grid(row=0, column=1)
 
+#create output layout
+my_listbox = tkinter.Listbox(output_frame, width=75,height=14)
+my_listbox.grid(row=0, column=0)
+
+
+#create button frame
+list_remove_button = tkinter.Button(button_frame, text="Remove", bg="#DCCDE8", borderwidth=2)
+list_clear_button = tkinter.Button(button_frame, text="Clear", bg="#DCCDE8", borderwidth=2)
+save_button = tkinter.Button(button_frame, text="Save", bg='#DCCDE8', borderwidth=2)
+quit_button = tkinter.Button(button_frame, text="Close", bg="#DCCDE8", borderwidth=2, command=root.destroy)
+list_remove_button.grid(row=0, column=0)
+list_clear_button.grid(row=0, column=1)
+save_button.grid(row=0, column=2)
+quit_button.grid(row=0, column=3)
+
+
 root.mainloop()
