a2d8064b280588edd66fe751bb6607bfd8216be5
Soham Das <majumder70.ranu@gmail.com>
2020-10-10 09:08:34 +0530

    Drop down menu Added

--- metric-helper.py
+++ metric-helper.py
@@ -1,5 +1,5 @@
 import tkinter
-from tkinter import StringVar
+from tkinter import ttk
 root = tkinter.Tk()
 root.title('Metric Helper')
 root.iconbitmap('wave.ico')
@@ -13,28 +13,26 @@ input_field = tkinter.Entry(root, width=20)
 output_field = tkinter.Entry(root, width=20)
 equal_field = tkinter.Label(root, text="=", bg="#1B998B")
 
-input_field.grid(row=0, column = 0)
-output_field.grid(row=0, column=2)
-equal_field.grid(row=0, column=1)
+input_field.grid(row=0, column = 0, padx=10, pady=10)
+output_field.grid(row=0, column=2, padx=10, pady=10)
+equal_field.grid(row=0, column=1, padx=10, pady=10)
 
 input_field.insert(0, "Enter your Quantity")
 
 metric_list = ["femto", "pico", "nano", "micor", "milli", "centi", "deci", "kilo", "hecto", "mega", "giga", "tera"]
-input_choice = StringVar()
-output_choice = StringVar()
-input_drop = tkinter.OptionMenu(root, input_choice, *metric_list)
-output_drop = tkinter.OptionMenu(root, output_choice, *metric_list)
+input_combobox = ttk.Combobox(root, value=metric_list, justify='center')
 to_Label = tkinter.Label(root, text="to")
+output_combobox = ttk.Combobox(root, value=metric_list, justify='center')
 
-input_drop.grid(row=1, column=0)
+input_combobox.grid(row=1, column=0, padx=10, pady=10)
 to_Label.grid(row=1, column=1)
-output_drop.grid(row=1, column=2)
+output_combobox.grid(row=1, column=2, padx=10, pady=10)
 
-input_choice.set("centi")
-output_choice.set('milli')
+input_combobox.set("centi")
+output_combobox.set('milli')
 
 
 convert_button = tkinter.Button(root, text="Convert", bg="#DBB4AD")
-convert_button.grid(row=2, column=0, columnspan=3)
+convert_button.grid(row=2, column=0, columnspan=3, padx=10, pady=10, ipadx=50)
 
 root.mainloop()
