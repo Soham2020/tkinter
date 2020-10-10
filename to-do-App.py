82f3b7d5c86cddbb1ef4f6e9223ad02cee984f92
Soham Das <majumder70.ranu@gmail.com>
2020-10-10 20:38:52 +0530

    added functions of scrollbar and remove button

--- to-do-App.py
+++ to-do-App.py
@@ -1,4 +1,6 @@
 import tkinter
+from tkinter import END, ANCHOR
+
 
 root = tkinter.Tk()
 root.title('To-do App')
@@ -6,6 +8,16 @@ root.iconbitmap('wave.ico')
 root.geometry('500x300')
 root.config(bg='#14BDEB')
 root.resizable(0,0)
+
+#define functions
+def add_item():
+    my_listbox.insert(END, list_entry.get())
+    list_entry.delete(0, END)
+
+def remove_item():
+    my_listbox.delete(ANCHOR)
+
+
 
 #create frames
 input_frame = tkinter.Frame(root, bg='#14BDEB')
@@ -17,18 +29,19 @@ button_frame.pack()
 
 #create input layout
 list_entry = tkinter.Entry(input_frame, borderwidth=3, width=35)
-list_add_button = tkinter.Button(input_frame, text='Add', bg="#DCCDE8", borderwidth=3)
+list_add_button = tkinter.Button(input_frame, text='Add', bg="#DCCDE8", borderwidth=3, command=add_item)
 list_entry.grid(row=0, column=0, padx=5, pady=5)
 list_add_button.grid(row=0, column=1, padx=5, pady=5)
 
 #create output layout
-my_listbox = tkinter.Listbox(output_frame, width=75,height=14)
 my_scrollbar = tkinter.Scrollbar(output_frame)
+my_listbox = tkinter.Listbox(output_frame, width=75,height=14, yscrollcommand=my_scrollbar.set)
+my_scrollbar.config(command=my_listbox.yview)
 my_listbox.grid(row=0, column=0)
 my_scrollbar.grid(row=0, column=1, sticky="NS")
 
 #create button frame
-list_remove_button = tkinter.Button(button_frame, text="Remove", bg="#DCCDE8", borderwidth=2)
+list_remove_button = tkinter.Button(button_frame, text="Remove", bg="#DCCDE8", borderwidth=2, command=remove_item)
 list_clear_button = tkinter.Button(button_frame, text="Clear", bg="#DCCDE8", borderwidth=2)
 save_button = tkinter.Button(button_frame, text="Save", bg='#DCCDE8', borderwidth=2)
 quit_button = tkinter.Button(button_frame, text="Close", bg="#DCCDE8", borderwidth=2, command=root.destroy)
