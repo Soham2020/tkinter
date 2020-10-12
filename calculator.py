699ead9b110dbd1824bb37d313b0905ab57bfd9d
Soham Das <majumder70.ranu@gmail.com>
2020-10-12 19:52:18 +0530

    added the layout

---
+++ calculator.py
@@ -0,0 +1,10 @@
+import tkinter
+
+root = tkinter.Tk()
+root.title('Calculator')
+root.iconbitmap('wave.ico')
+root.geometry('400x400')
+root.resizable(0,0)
+root.config(bg="#169873")
+
+root.mainloop()
