d75ba21cca283fd5566a62eee214eaadebc326e9
Soham Das <majumder70.ranu@gmail.com>
2020-10-12 20:01:05 +0530

    define frames

--- calculator.py
+++ calculator.py
@@ -7,4 +7,13 @@ root.geometry('400x400')
 root.resizable(0,0)
 root.config(bg="#169873")
 
+
+
+#define frames
+display_frame = tkinter.LabelFrame(root)
+button_frame = tkinter.LabelFrame(root)
+display_frame.pack()
+button_frame.pack()
+
+
 root.mainloop()
